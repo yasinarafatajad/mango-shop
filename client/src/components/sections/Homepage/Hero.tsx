c140e5e1501e9de6fa353ee9277a5166a7b8c7e5
Yasin Arafat Ajad <yasinarafatazad082@gmail.com>
2026-05-16 13:06:53 +0600

    [UI]: update hero section design and content

--- client/src/components/sections/Homepage/Hero.tsx
+++ client/src/components/sections/Homepage/Hero.tsx
@@ -1,12 +1,34 @@
+import bannerImg from "@/assets/hero-section/hero-banner-bg.png";
+import Image from "next/image";
+import './hero.css';
+
 ;
 
 export default function Hero() {
   return (
-    <section className="banner-wrapper">
-      <div className="banner">
-        <h2>সেরা আম সরাসরি বাগান থেকে!</h2>
-        <p>এখনই অর্ডার করুন সেরা অফারে ।</p>
-        <div className="banner-img">🥭</div>
+    <section className="hero-section">
+      {/* Background */}
+      <div className="hero-bg">
+        <Image
+          src={bannerImg}
+          alt="Nature's Bounty Banner"
+          fill
+          priority
+          className="hero-img"
+        />
+
+        <div className="hero-overlay" />
+      </div>
+
+      {/* Content */}
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1></h1>
+
+          <h2>সেরা আম সরাসরি বাগান থেকে!</h2>
+
+          <p>এখনই অর্ডার করুন সেরা অফারে ।</p>
+        </div>
       </div>
     </section>
   );
